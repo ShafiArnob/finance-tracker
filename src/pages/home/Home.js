8a9cd8dde28cba83539c1215b38c4a2fd7a18f71
ShafiArnob <shafiarnob@gmail.com>
2022-09-16 02:31:31 +0600

    routing

---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+
+
+//css
+import './Home.module.css'
+
+function Home() {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
---
+++ src/pages/signup/SignUp.js
@@ -0,0 +1,11 @@
+
+//css
+import './SignUp.module.css'
+
+function SignUp() {
+  return (
+    <div>SignUp</div>
+  )
+}
+
+export default SignUp
