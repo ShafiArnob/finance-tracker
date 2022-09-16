fc5dde256a40a93e4474a58543afe00369770494
ShafiArnob <shafiarnob@gmail.com>
2022-09-16 12:17:03 +0600

    signup

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB4NlJKU5qkVp-cW8niqJ4UE2_DCQdD2Do",
+  authDomain: "finance-tracker-eebce.firebaseapp.com",
+  projectId: "finance-tracker-eebce",
+  storageBucket: "finance-tracker-eebce.appspot.com",
+  messagingSenderId: "444612622595",
+  appId: "1:444612622595:web:7f62b537f24b9025cc5136"
+};
