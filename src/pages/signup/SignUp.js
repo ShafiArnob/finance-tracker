4ad8c7e651d7f772f842f385cd483c4d74e464e3
ShafiArnob <shafiarnob@gmail.com>
2022-09-19 23:28:03 +0600

    signup

--- src/pages/signup/SignUp.js
+++ src/pages/signup/SignUp.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import {useSignup} from '../../hooks/useSignup'
 
 // styles
 import styles from './SignUp.module.css'
@@ -8,9 +9,11 @@ export default function Signup() {
   const [password, setPassword] = useState('')
   const [displayName, setDisplayName] = useState('')
 
+  const {signup, isPending, error} = useSignup()
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(email, password, displayName)
+    signup(email, password, displayName)
   }
 
   return (
@@ -18,29 +21,20 @@ export default function Signup() {
       <h2>sign up</h2>
       <label>
         <span>email:</span>
-        <input 
-          type="email" 
-          onChange={(e) => setEmail(e.target.value)} 
-          value={email}
-        />
+        <input type="email" onChange={(e) => setEmail(e.target.value)} value={email}/>
       </label>
       <label>
         <span>password:</span>
-        <input 
-          type="password" 
-          onChange={(e) => setPassword(e.target.value)} 
-          value={password} 
-        />
+        <input type="password" onChange={(e) => setPassword(e.target.value)} value={password}/>
       </label>
       <label>
         <span>display name:</span>
-        <input 
-          type="text" 
-          onChange={(e) => setDisplayName(e.target.value)}
-          value={displayName}
-        />
+        <input type="text" onChange={(e) => setDisplayName(e.target.value)} value={displayName}/>
       </label>
-      <button className="btn">Sign up</button>
+      
+      {!isPending && <button className="btn">Sign up</button>}
+      {isPending && <button className='btn' disabled>Loading...</button>}
+      {error && <p>{error}</p>}
     </form>
   )
 }
