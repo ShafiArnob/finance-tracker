cc0b3ad01d9e2df58d931bcd5879c20b90e88b09
ShafiArnob <shafiarnob@gmail.com>
2022-09-22 23:44:51 +0600

    transaction form

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,19 @@
 
 
 //css
-import './Home.module.css'
+import styles from'./Home.module.css'
+import TransactionForm from './TransactionForm'
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        TransList
+      </div>
+
+      <div className={styles.sidebar}>
+        <TransactionForm/>
+      </div>
     </div>
   )
 }
