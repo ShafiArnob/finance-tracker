86957918e38509f37521d60407b6b9081f296fb4
ShafiArnob <shafiarnob@gmail.com>
2022-09-26 00:22:17 +0600

    firebase user query

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,12 +1,16 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef} from "react"
 import { projectFirestore } from "../firebase/config"
 
-export const useCollection = (collection) =>{
+export const useCollection = (collection, _query ) =>{
   const [documents, setDocuments] = useState(null)
   const [error, setError] = useState()
-
+  const query = useRef(_query).current
   useEffect(()=>{
     let ref = projectFirestore.collection(collection)
+
+    if(query){
+      ref = ref.where(...query)
+    }
 
     const unsubscribe = ref.onSnapshot((snapshot)=>{
       let results = []
@@ -25,7 +29,7 @@ export const useCollection = (collection) =>{
     //unsubscribe on unmount
     return () => unsubscribe()
     
-  }, [collection])
+  }, [collection, query])
 
 
   return {documents, error}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -10,7 +10,7 @@ import TransactionForm from './TransactionForm'
 
 function Home() {
   const{user} = useAuthContext()
-  const {documents, error} = useCollection('transaction')
+  const {documents, error} = useCollection('transaction',["uid","==",user.uid])
   // console.log(documents);
   return (
     <div className={styles.container}>
