526cccc26b4b12120de1afe51c95e77312c9a779
ShafiArnob <shafiarnob@gmail.com>
2022-09-26 22:31:47 +0600

    delete transaction

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -15,6 +15,8 @@ const firestoreReducer = (state, action) =>{
       return {isPending:true, document:null, success: false, error:null}
     case 'ADDED_DOCUMENT':
       return {isPending:false, document:action.payload, success: true, error:null}
+    case 'DELETED_DOCUMENT':
+      return {isPending:false, document:null, success: true, error:null}
     case 'ERROR':
       return {isPending:false, document:null, success: false, error:action.payload}
     default:
@@ -53,8 +55,15 @@ const useFirestore = (collection) => {
   }
 
   //delete document 
-  const deleteDocument = () =>{
+  const deleteDocument = async (id) =>{
+    dispatch({type: "IS_PENDING"})
 
+    try{
+      await ref.doc(id).delete()
+      dispatchIfNotCancelled({type:"DELETED_DOCUMENT"})
+    }catch(err){
+      dispatchIfNotCancelled({type:"ERROR", payload:"Could not delete"})
+    }
   }
 
   useEffect(()=>{
--- src/pages/home/TransactionList.jsx
+++ src/pages/home/TransactionList.jsx
@@ -1,8 +1,11 @@
 
 //css
+import { useFirestore } from '../../hooks/useFirestore'
 import styles from './Home.module.css'
 
 function TransactionList({transactions}) {
+  const {deleteDocument, response} = useFirestore("transaction")
+  console.log(response);
   // console.log(transactions);
   return (
     <ul className={styles.transactions}>
@@ -10,6 +13,7 @@ function TransactionList({transactions}) {
         <li key={transaction.id}>
           <p className={styles.name}>{transaction.name}</p>
           <p className={styles.amount}>${transaction.amount}</p>
+          <button onClick={()=>deleteDocument(transaction.id)}>X</button>
         </li>
       )}
     </ul>
